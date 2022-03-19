a3f6c9ec53fba6ef9fefe01df0c43872ab237cd7
Timothy Chin <chinti@bc.edu>
2022-03-19 00:14:14 -0400

    Section 6.7

--- weather-gift/LocationDetailViewController.swift
+++ weather-gift/LocationDetailViewController.swift
@@ -27,7 +27,29 @@ class LocationDetailViewController: UIViewController {
             
         }
         
+        loadLocations()
         updateUserInterface()
+    }
+    
+    func loadLocations() {
+        
+        guard let locationsEncoded = UserDefaults.standard.value(forKey: "weatherLocations") as? Data else {
+                    
+                    print("⚠️Warning: Could not load weatherLocations data from UserDefaults. This would always be the case the first time an app is installed, so if that's the case, ignore this error.")
+                    return
+        }
+        
+        let decoder = JSONDecoder()
+        if let weatherLocations = try? decoder.decode(Array.self, from: locationsEncoded) as [WeatherLocation] {
+            
+            self.weatherLocations = weatherLocations
+            
+        } else {
+            
+            print("Error: Couldn't decode data read from UserDefaults")
+            
+        }
+        
     }
     
     func updateUserInterface() {
--- weather-gift/LocationListViewController.swift
+++ weather-gift/LocationListViewController.swift
@@ -32,8 +32,24 @@ class LocationListViewController: UIViewController {
 
     }
     
+    func saveLocations() {
+        
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(weatherLocations) {
+            
+            UserDefaults.standard.set(encoded, forKey: "weatherLocations")
+            
+        } else {
+            
+            print("🤬 Error: Saving encoded didn't work!")
+            
+        }
+        
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         selectedLocationIndex = tableView.indexPathForSelectedRow!.row
+        saveLocations()
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
