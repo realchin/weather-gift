9285b39aaf8bb91710c2d45448835114e4f35764
Timothy Chin <chinti@bc.edu>
2022-03-25 18:10:08 -0400

    Section 6.10

--- weather-gift/WeatherLocation.swift
+++ weather-gift/WeatherLocation.swift
@@ -21,4 +21,44 @@ class WeatherLocation: Codable {
         
     }
     
+    func getData() {
+//        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=42.335525&lon=-71.168645&appid=40ed7d887d778cd3a7958897d80f4f20"
+    
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
+        
+        print("We are accessing the url \(urlString)")
+        
+        // Create a URL
+        guard let url = URL(string: urlString) else {
+            print("ERROR! Could not create a URL from \(urlString)")
+            return
+        }
+        
+        // Create session
+        
+        let session = URLSession.shared
+        
+        // get data with .dataTask method
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            
+            // note: there are some additional things that could go wrong when using URLSession, but we shouldnt experience them, so we will ignore testing for these for now...
+            
+            // deal with the data
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("\(json)")
+            } catch {
+                print("JSON Error: \(error.localizedDescription)")
+            }
+        }
+    
+        task.resume()
+        
+    }
+    
 }
