3eddd112e8b7c22743a969a637041adff3e5813e
Timothy Chin <chinti@bc.edu>
2022-03-25 18:25:43 -0400

    Section 6.11

--- weather-gift/LocationDetailViewController.swift
+++ weather-gift/LocationDetailViewController.swift
@@ -15,7 +15,7 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
     
-    var weatherLocation: WeatherLocation!
+    var weatherDetail: WeatherDetail!
     var locationIndex = 0
     
     override func viewDidLoad() {
@@ -27,17 +27,20 @@ class LocationDetailViewController: UIViewController {
     func updateUserInterface() {
         
         let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
-        weatherLocation = pageViewController.weatherLocations[locationIndex]
-        
-        dateLabel.text = ""
-        placeLabel.text = weatherLocation.name
-        temperatureLabel.text = "__°"
-        summaryLabel.text = ""
+        let weatherLocation = pageViewController.weatherLocations[locationIndex]
+        weatherDetail = WeatherDetail(name: weatherLocation.name, latitude: weatherLocation.longitude, longitude: weatherLocation.latitude)
         
         pageControl.numberOfPages = pageViewController.weatherLocations.count
         pageControl.currentPage = locationIndex
         
-        weatherLocation.getData()
+        weatherDetail.getData {
+            DispatchQueue.main.async {
+                self.dateLabel.text = self.weatherDetail.timezone
+                self.placeLabel.text = self.weatherDetail.name
+                self.temperatureLabel.text = "\(self.weatherDetail.temperature)°"
+                self.summaryLabel.text = self.weatherDetail.summary
+            }
+        }
         
     }
     
