b2a12a4effc5e1f748f332861d5bc084a6a7fcab
Timothy Chin <chinti@bc.edu>
2022-03-26 19:18:50 -0400

    Section 6.21

--- weather-gift/LocationListViewController.swift
+++ weather-gift/LocationListViewController.swift
@@ -108,7 +108,19 @@ extension LocationListViewController: UITableViewDataSource, UITableViewDelegate
         
     }
     
+    // Mark: -tableView methods to freeze the first cell
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return (indexPath.row != 0 ? true : false)
+    }
     
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return (indexPath.row != 0 ? true : false)
+
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+    }
 }
 
 extension LocationListViewController: GMSAutocompleteViewControllerDelegate {
